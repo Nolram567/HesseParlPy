5fe7cd07386b5cc453d1af98a5f695e9e7887034
Nolram567 <lucif3r@posteo.de>
2024-07-19 16:15:33 +0200

    update fig generation

--- topic_miner.py
+++ topic_miner.py
@@ -34,33 +34,33 @@ if __name__ == "__main__":
 
     label_id_map = {
         1: "Gesetzgebung",
-        2: "Covid-19-Pandemie",
+        2: "Covid-19-Pandemie_I",
         3: "Medizinische Versorgung",
-        4: "Covid-19-Pandemie",
+        4: "Covid-19-Pandemie_II",
         5: "Kultur",
         6: "Justizwesen",
         7: "Hochschulwesen",
-        8: "Landesfinanzen",
+        8: "Landesfinanzen_I",
         9: "(Erneuerbare) Energie",
-        10: "Covid-19-Pandemie",
+        10: "Covid-19-Pandemie_III",
         11: "Barrierefreiheit",
         12: "ÖRR",
         13: "Europäische Union",
-        14: "Verwaltung im Landtag",
+        14: "Verwaltung im Landtag_I",
         15: "Gewalt/Polizei*",
         16: "Familie",
         17: "Wirtschaft",
         18: "Russischer Angriffskrieg",
-        19: "Landesfinanzen",
+        19: "Landesfinanzen_II",
         20: "Kommunalwesen",
-        21: "Verwaltung im Landtag",
-        22: "**",
+        21: "Verwaltung im Landtag_II",
+        22: "**_I",
         23: "Wohnraum",
         24: "Terrorismus",
         25: "Migration und Asyl",
         26: "Demokratie",
         27: "Schulwesen",
-        28: "**",
+        28: "**_II",
         29: "Vereinswesen"
     }
 
@@ -70,23 +70,35 @@ if __name__ == "__main__":
             topic_correlations[key] = correlation_matrix[i, j]
 
     # Ausgabe der Korrelationen
-    print(topic_correlations)
+    #print(topic_correlations)
     labels, values = zip(*topic_correlations.items())
 
     # Boxplot erstellen
+    fig = go.Figure()
+
+    # Berechnung der Perzentile
+    lower_whisker = np.percentile(values, 2.5)
+    upper_whisker = np.percentile(values, 97.5)
+
+    print(upper_whisker, lower_whisker)
+    # Erstellung des Boxplots mit benutzerdefinierten Whiskern
     fig = go.Figure()
 
     fig.add_trace(go.Box(y=values,
                          boxpoints='all',  # Alle Punkte anzeigen
                          jitter=0.5,  # Punkte horizontal streuen
                          pointpos=-1.8,  # Position der Punkte relativ zum Boxplot
-                         #hovertemplate= f"{label, value for label, value in zip(labels, values)}",
-                         hovertext= [f'{label}: {value}' for label, value in zip(labels, values)],  # Tooltips mit den Themenlabels
+                         # hovertemplate= f"{label, value for label, value in zip(labels, values)}",
+                         hovertext=[f'{label}: {value}' for label, value in zip(labels, values)],
+                         # Tooltips mit den Themenlabels
                          marker_color='blue',
-                         boxmean='sd',
                          name="Pearson-Korrelation"))
-
     # Layout-Anpassungen
-    fig.update_layout(title='Korrelationen zwischen Themen',
-                      yaxis_title='Pearson-Korrelationskoeffizient',
-                      showlegend=False)
+    fig.update_layout(
+        title='Korrelationen zwischen Themen',
+        yaxis_title='Pearson-Korrelationskoeffizient',
+        showlegend=False
+    )
+
+    # Diagramm anzeigen
+    fig.show()
